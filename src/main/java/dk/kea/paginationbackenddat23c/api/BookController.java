059a14f4d35461b1ecad3da94cb4a285e0885623
Jarl Tuxen <jart@kea.dk>
2024-11-19 14:34:19 +0100

    removed CrossOrigin in controller

--- src/main/java/dk/kea/paginationbackenddat23c/api/BookController.java
+++ src/main/java/dk/kea/paginationbackenddat23c/api/BookController.java
@@ -29,7 +29,7 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 //@RequestMapping("/api/books")
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*") //skal fjernes når config i Spring Security
 public class BookController {
 
     private BookService bookService;
